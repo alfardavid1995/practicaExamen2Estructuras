88657311b4d849817ea93eebc837b7d1ad5c61c8
David Alfaro <alfardavid95@gmail.com>
2022-11-09 20:24:33 -0600

    inicializar la ostia

---
+++ src/practicaexamen2estructuras/PracticaExamen2Estructuras.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicaexamen2estructuras;
+
+/**
+ *
+ * @author css124646
+ */
+public class PracticaExamen2Estructuras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
