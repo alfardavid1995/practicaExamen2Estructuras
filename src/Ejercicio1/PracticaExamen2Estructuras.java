77015fdb3236880fbd92711fc7cf94e8b8e42f67
David Alfaro <alfardavid95@gmail.com>
2022-11-10 02:26:47 -0600

    corregir el puto bug que me tenia de los huevos

--- src/Ejercicio1/PracticaExamen2Estructuras.java
+++ src/Ejercicio1/PracticaExamen2Estructuras.java
@@ -6,17 +6,36 @@ public class PracticaExamen2Estructuras {
 
     public static void main(String[] args) {
 
-        String palabra = JOptionPane.showInputDialog("Digite una palabra");
+        //String palabra = JOptionPane.showInputDialog("Digite una palabra");
+        
+        String palabra = "caleidoscopio";
         //crear una lista  
         Lista lista = new Lista();
         //mandar un string para ser partido 
         
-        insertarEnLaLista(palabra,lista);
+        
+        insertarEnLaLista(palabra,lista);//inserta lista
+        System.out.println(lista.toString());//muestra toda la lista
+        testing(palabra, lista);
+        
          
-         int index = Integer.parseInt(JOptionPane.showInputDialog("Digite un el indice que desea consultar"));
+        
+        
+         //int index = Integer.parseInt(JOptionPane.showInputDialog("Digite un el indice que desea consultar"));
+
          
-         System.out.println(lista.retornarDatos(index));
+         
+         
+         
     }
+    
+    public static void testing(String palabra, Lista lista){
+        for (int i = 0; i < palabra.length(); i++) {
+            System.out.println("Valor del dato '"+ palabra.charAt(i)+ "' indice del dato: "+i);
+            System.out.print(lista.retornarDatos(i));
+        }
+    }
+    
     public static Lista insertarEnLaLista(String palabra, Lista lista){
         for (int i = 0; i < palabra.length(); i++) {
             char c = palabra.charAt(i);
