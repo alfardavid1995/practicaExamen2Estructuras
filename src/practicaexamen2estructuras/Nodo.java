8a44d8ae06595b69bfceb5fb5aa72357c7f88cf9
AleMendezS <98509409+AleMendezS@users.noreply.github.com>
2022-11-09 20:30:25 -0600

    se agrega las clases correspondientes

---
+++ src/practicaexamen2estructuras/Nodo.java
@@ -0,0 +1,31 @@
+package practicaexamen2estructuras;
+
+public class Nodo {
+    private Casa dato;
+    private Nodo next;
+    
+    @Override
+    public String toString(){
+        return "Nodo{"+"dato="+dato+'}';
+    }
+    
+    public Nodo(Casa dato){
+        this.dato=dato;
+    }
+
+    public Casa getDato() {
+        return dato;
+    }
+
+    public void setDato(Casa dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getNext() {
+        return next;
+    }
+
+    public void setNext(Nodo next) {
+        this.next = next;
+    }
+}
