067d4573868bc6598e8f4153169a963537a6bc3a
David Alfaro <alfardavid95@gmail.com>
2022-11-13 02:21:56 -0600

    lista doblemente enlazada

--- src/Ejercicio2/ListaCircularDoble.java
+++ src/Ejercicio2/ListaCircularDoble.java
@@ -31,15 +31,17 @@ public class ListaCircularDoble {
         ultimo.setNext(cabeza);
         cabeza.setBack(ultimo); 
     }
+    
+    
 // este es el insert que vimos en clase
 public void insert(Persona personitaInsertada){
-    //en caso que la lista doble este vacia
-    if(cabeza==null){//si la lista esta vacia solo queda asigndar i listo 
+
+    if(cabeza==null){//si la lista esta vacia solo queda asigndar el nodo con los parametros y listo  
         cabeza = new Nodo(personitaInsertada);//mando el nodo a cabeza
         ultimo = cabeza;//lo igualo la cabeza 
         ultimo.setNext(cabeza);//enlazo el ultimo a la cabeza 
         cabeza.setBack(ultimo);//enlazo el primero al ultimo, y ahi es donde la lista se vuelve ciercular 
-    }else if(personitaInsertada.getId()<cabeza.getDato().getId()){
+    }else if(personitaInsertada.getId()>cabeza.getDato().getId()){// si el dato de la cabeza es menor a 
         // si lo que voy a insertar esta a la izquiera de la cabeza (en otras palabras ir a back y no a next)
         Nodo aux = new Nodo(personitaInsertada);//creo el nodo auxiliar con el parametro recien creado
         aux.setNext(cabeza);//pongo el valor aux y le pongo que la cabeza es el siguiente, coloco aux a la izquierda de la cabeza
@@ -53,27 +55,95 @@ public void insert(Persona personitaInsertada){
         ultimo.setNext(cabeza);//enlazo ultimo a la cabeza (circular)
         cabeza.setBack(ultimo);// enlazo cabeza al ultimo (circular)   
     }else{
-        Nodo aux = cabeza;
+        Nodo aux = cabeza;//defino aux como cabeza
+        //mientras el dato del siguiente sea menor que el dato que voy a insertar,
+        //insertar avance en la lista
         
         while (aux.getNext().getDato().getId()<personitaInsertada.getId()){
             aux= aux.getNext();
         }
-        Nodo temp = new Nodo(personitaInsertada);
-        temp.setNext(aux.getNext());
-        temp.setBack(aux);
-        aux.setNext(temp);
-        temp.getNext().setBack(temp);
+        // ya una vez ubicado procede a hacer el enlazamiento que corresponde
+        Nodo temp = new Nodo(personitaInsertada);// creo nodo temp para crear el nodo con p
+        temp.setNext(aux.getNext());// enlazo temp al siguiente de aux
+        temp.setBack(aux);//enlazo temp al de atras
+        aux.setNext(temp);//enlazo el siguiente de aux como temp
+        temp.getNext().setBack(temp);// enlazo el siguiente de temp
     }
-}    
+}
+
+    public void insertaTavoModificado(Persona p){
+        if(cabeza==null){
+            cabeza = new Nodo(p);
+            ultimo = cabeza;
+        }else if(p.getId()<17){
+            //si lo que voy a insertar debe estar a la izquierda de cabeza
+            Nodo aux = new Nodo(p);//creo el nodo aux con el parametro 
+            aux.setNext(cabeza);//pongo el nodo aux a la izquierda de la cabeza
+            cabeza=aux;//cabeza es el 
+        }else if(18<= p.getId()){
+            ultimo.setNext(new Nodo(p));
+            ultimo = ultimo.getNext();
+        }else{
+           Nodo aux = cabeza;
+           while (aux.getNext().getDato().getId() < p.getId()){
+               aux=aux.getNext();
+           }
+           Nodo temp = new Nodo(p);
+           temp.setNext(aux.getNext());
+           temp.setBack(aux);
+           aux.setNext(temp);
+           temp.getNext().setBack(temp); //nuevo
+        }
+        ultimo.setNext(cabeza);
+        cabeza.setBack(ultimo); 
+    }
+
+public void metodoModificadoDavid(Persona personitaInsertada){
+
+    if(cabeza==null){//si la lista esta vacia solo queda asigndar el nodo con los parametros y listo  
+        cabeza = new Nodo(personitaInsertada);//mando el nodo a cabeza
+        ultimo = cabeza;//lo igualo la cabeza 
+        ultimo.setNext(cabeza);//enlazo el ultimo a la cabeza 
+        cabeza.setBack(ultimo);//enlazo el primero al ultimo, y ahi es donde la lista se vuelve ciercular 
+    }else if(personitaInsertada.getId()>cabeza.getDato().getId()){
+        // si lo que voy a insertar esta a la izquiera de la cabeza (en otras palabras ir a back y no a next)
+        Nodo aux = new Nodo(personitaInsertada);//creo el nodo auxiliar con el parametro recien creado
+        aux.setNext(cabeza);//pongo el valor aux y le pongo que la cabeza es el siguiente, coloco aux a la izquierda de la cabeza
+        cabeza = aux; //cabeza ahora referencia al nuevo elemento 
+        ultimo.setNext(cabeza);//enlazo el ultimo con la cabeza
+        cabeza.setBack(ultimo);//enlazo cabeza con el ultimo (circular)
+    }else if (ultimo.getDato().getId()<=personitaInsertada.getId()){
+        //si debo insertar  al final de la lista 
+        ultimo.setNext(new Nodo(personitaInsertada));//llamo al next del ultimo directamente y le pongo un nodo nuevo ahi 
+        ultimo = ultimo.getNext();//ultimo se actualiza en nuevo nodo 
+        ultimo.setNext(cabeza);//enlazo ultimo a la cabeza (circular)
+        cabeza.setBack(ultimo);// enlazo cabeza al ultimo (circular)   
+    }else{
+        Nodo aux = cabeza;//defino aux como cabeza
+        //mientras el dato del siguiente sea menor que el dato que voy a insertar,
+        //insertar avance en la lista
+        
+        while (aux.getNext().getDato().getId()<personitaInsertada.getId()){
+            aux= aux.getNext();
+        }
+        // ya una vez ubicado procede a hacer el enlazamiento que corresponde
+        Nodo temp = new Nodo(personitaInsertada);// creo nodo temp para crear el nodo con p
+        temp.setNext(aux.getNext());// enlazo temp al siguiente de aux
+        temp.setBack(aux);//enlazo temp al de atras
+        aux.setNext(temp);//enlazo el siguiente de aux como temp
+        temp.getNext().setBack(temp);// enlazo el siguiente de temp
+    }
+}
+
     @Override
     public String toString(){
         Nodo aux = cabeza;
-        String s="Lista: ";
+        String s="Lista: \n";
         if (aux != null){
-            s+=aux+", ";
+            s+=aux+",\n ";
             aux=aux.getNext();
             while(aux != cabeza){
-                s+=aux+", ";
+                s+=aux+",\n ";
                 aux=aux.getNext();
             }
         } else{
--- src/Ejercicio2/MainEjercicio2.java
+++ src/Ejercicio2/MainEjercicio2.java
@@ -9,12 +9,32 @@ package Ejercicio2;
  * @author css124646
  */
 public class MainEjercicio2 {
-
+public static ListaCircularDoble listaCircularDoble =  new ListaCircularDoble ();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+
+        
+        insertarpersona (new Persona(20, "Mengano"));
+        insertarpersona (new Persona(14, "Sutano"));
+        insertarpersona (new Persona(25, "Perensejo"));
+        insertarpersona (new Persona(23, "Fulano"));
+        insertarpersona (new Persona(24, "Pedro el escamoso"));
+        insertarpersona (new Persona(8, "Betty la fea"));
+        insertarpersona (new Persona(30, "Sheldon"));
+        insertarpersona (new Persona(3, "Bob Esponja"));
+        
+
+        
+        System.out.println(listaCircularDoble.toString());    
+    }
+    
+    public static void  insertarpersona(Persona persona){
+            listaCircularDoble.insertaTavoModificado(persona);
+
     }
     
 }
